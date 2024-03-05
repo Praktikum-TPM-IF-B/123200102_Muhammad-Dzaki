7d7f111165b5ae02065c243b8b0c0a9b8d08b3f0
M.Dzaki <72814435+MHMMDDzaki@users.noreply.github.com>
2024-03-05 08:15:24 +0700

    pertemuan 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Login Page',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,10 +30,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.tealAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Login Page'),
     );
   }
 }
@@ -105,13 +107,48 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              child: const Text(
+                'Login',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              // Text(
+              //   '$_counter',
+              //   style: Theme.of(context).textTheme.headlineMedium,
+              // ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(
+              padding: EdgeInsets.all(15),
+              child:
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'username'
+                ),
+              ),
             ),
+            Container(
+              padding: EdgeInsets.all(15),
+              child:
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'password'
+                ),
+              )
+            ),
+            Container(
+              padding: EdgeInsets.all(0),
+              child:
+                ElevatedButton(
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.lightBlueAccent)
+                  ),
+                  onPressed: () {},
+                  child: Text("Login"),
+                ),
+            )
+
           ],
         ),
       ),
